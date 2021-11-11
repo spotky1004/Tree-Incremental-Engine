95fa3e34f0ac09be44619fe766dd9ec2dec54f69
Spotky1004 <spotky1004@gmail.com>
2021-11-11 16:34:03 +0900

    feat: Update demo

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,132 @@
-import Incremental from "../index.js";
+import {
+  Incremental,
+  Layer,
+  Resource,
+  Upgrade,
+  Upgrades,
+} from "../index.js";
+import Decimal from "./lib/decimal.js";
 
-let game = new Incremental();
-let store = game.store;
+const game = new Incremental();
+
+const resources = {
+  tree: new Resource({
+    name: "Tree",
+  }),
+  apple: new Resource({
+    name: "Apple",
+  })
+};
+
+game.attachResource(resources.tree);
+
+const treeLayerUpgrade = new Upgrade({
+  title: "GMO Seed",
+  description: "Make seed grow faster (x1.2 tree/s per buy)",
+  resource: resources.apple,
+  cost: [
+    [
+      "Linear", {
+        start: 10,
+        increment: 10,
+      }
+    ],
+    [
+      "Exponential", {
+        start: 300,
+        mul: 1.3
+      }
+    ]
+  ]
+});
+const treeLayerUpgrades = new Upgrades({
+  resource: resources.apple,
+  upgrades: [
+    {
+      title: "Bigger 🌲",
+      description: "Bigger 🌲 make 🍎 production x2",
+      cost: 1e3
+    },
+    {
+      title: "Faster 🌲",
+      description: "Faster 🌲 make 🍎 production x3",
+      cost: 1e4
+    },
+    {
+      title: "🌲 with more branches",
+      description: "Double the 🌲's branch, make 🍎 production x4",
+      cost: 1e6
+    },
+    {
+      title: "Shrink 🌲",
+      description: "Shrink 🌲 to make more space, make 🍎 production x5",
+      cost: 1e8
+    },
+    {
+      title: "Better 🌲",
+      description: "Better 🌲 make 🍎 production x6",
+      cost: 1e10
+    },
+    {
+      title: "More 🌲",
+      description: "Much more 🌲 make 🍎 production x7",
+      cost: 1e13
+    },
+    {
+      title: "🌧️ & 🌲",
+      description: "🌧️ makes 🌲 faster, make 🍎 production x8",
+      cost: 1e16
+    },
+    {
+      title: "👀 + 🌲",
+      description: "Make 👀 to 🌲 to find better place to make 🍎 production x9",
+      cost: 1e20
+    }
+  ]
+});
+const treeLayer = new Layer({
+  name: "Tree Layer",
+  mainResource: resources.apple,
+})
+  .attachContent(treeLayerUpgrade)
+  .attachContent(treeLayerUpgrades);
+
+game.attachLayer(treeLayer);
+
+const store = game.store;
+const save = store.save;
+
+const elememts = {
+  treeCount: document.getElementById("tree-count"),
+  appleCount: document.getElementById("apple-count")
+}
+
+let lastTick = new Date().getTime();
+function tick() {
+  const timeNow = new Date().getTime();
+  const dt = (timeNow - lastTick)/1000;
+  lastTick = timeNow;
+
+  const upgradeSave = save.layers[treeLayer.key].Upgrade;
+  const upgradesSave = save.layers[treeLayer.key].Upgrades;
+  
+  let treeProduction = new Decimal(0.1).mul(new Decimal(1.2).pow(upgradeSave[0]));
+  let appleProduction = save.resources[resources.tree.key].floor();
+  if (upgradesSave[0][0].eq(1)) appleProduction = appleProduction.mul(2);
+  if (upgradesSave[0][1].eq(1)) appleProduction = appleProduction.mul(3);
+  if (upgradesSave[0][2].eq(1)) appleProduction = appleProduction.mul(4);
+  if (upgradesSave[0][3].eq(1)) appleProduction = appleProduction.mul(5);
+  if (upgradesSave[0][4].eq(1)) appleProduction = appleProduction.mul(6);
+  if (upgradesSave[0][5].eq(1)) appleProduction = appleProduction.mul(7);
+  if (upgradesSave[0][6].eq(1)) appleProduction = appleProduction.mul(8);
+  if (upgradesSave[0][7].eq(1)) appleProduction = appleProduction.mul(9);
+
+  store.changeResourceAmount(resources.tree, treeProduction.mul(dt));
+  elememts.treeCount.innerText = `You have ${save.resources[resources.tree.key].floor().toNumber()} 🌲 (+${treeProduction.toNumber()}/s)`;
+  store.changeResourceAmount(resources.apple, appleProduction.mul(dt));
+  elememts.appleCount.innerText = `You have ${save.resources[resources.apple.key].floor().toNumber()} 🍎 (+${appleProduction.toNumber()}/s)`;
+
+  requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import Incremental from "./src/class/Incremental.js";
-
-export default Incremental;
+export { default as Incremental } from "./src/class/Incremental.js";
+export { default as Layer } from "./src/class/Layer.js";
+export { default as Upgrade } from "./src/class/contents/Upgrade.js";
+export { default as Upgrades } from "./src/class/contents/Upgrades.js";
+export { default as Resource } from "./src/class/contents/Rescouce.js";
+export { default as Cost } from "./src/class/base/Cost.js";
