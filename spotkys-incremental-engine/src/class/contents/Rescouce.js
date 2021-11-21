38a61bf0886653bc0764beeef674e5431d0a1c33
Spotky1004 <spotky1004@gmail.com>
2021-11-21 23:48:38 +0900

    chore: Add dispalyName to Resource

--- spotkys-incremental-engine/src/class/contents/Rescouce.js
+++ spotkys-incremental-engine/src/class/contents/Rescouce.js
@@ -4,6 +4,7 @@ import nameToKey from "../../util/nameToKey.js";
 /**
  * @typedef ResourceOptions
  * @property {string} name
+ * @property {string} [dispalyName]
  * @property {string | number} [initialAmount] - Set initial amount of this resource
  */
 
@@ -11,6 +12,7 @@ class Resource {
   /** @param {ResourceOptions} options */
   constructor(options) {
     this.name = options.name;
+    this.dispalyName = options.dispalyName ?? options.name;
     this.key = nameToKey(this.name.replace(/ /g, "_").toLowerCase());
 
     this.initialAmount = new Decimal(options.initialAmount ?? 0);
