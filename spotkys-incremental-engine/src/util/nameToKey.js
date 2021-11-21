603ff699492768379b7b762894fd3e4c290988c6
Spotky1004 <spotky1004@gmail.com>
2021-11-21 21:23:23 +0900

    chore: Better nameToKey function

--- spotkys-incremental-engine/src/util/nameToKey.js
+++ spotkys-incremental-engine/src/util/nameToKey.js
@@ -3,8 +3,7 @@
  * @return {string} 
  */
 function nameToKey(name) {
-  let key = name.replace(/[ +]/g, "_").replace(/[^A-z0-9_]/g, "").toLowerCase();
-  if (key.length === 0) key = `unknown_${name.split("").map(e => e.charCodeAt(0).toString(16).padStart(4, "0")).join("")}`;
+  let key = name.replace(/[ +]/g, "_").toLowerCase();
   return key;
 }
 
