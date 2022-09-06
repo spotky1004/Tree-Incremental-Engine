fef37107ed456fa104d7eabbf89a6526b0534fe3
Spotky1004 <spotky1004@gmail.com>
2022-09-06 15:44:36 +0900

    Add SIE.Resource

--- src/bundles/sie.ts
+++ src/bundles/sie.ts
@@ -1,9 +1,11 @@
 import Game from "../core/Game";
+import Resource from "../core/Resource";
 import conetnet from "./conetnet";
 import num from "./num";
 
 const SIE = {
   Game,
+  Resource,
   conetnet,
   num
 };
